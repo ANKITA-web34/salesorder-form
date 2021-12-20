543fa169338442c9a7861cea3997c86070bfd6ec
Ankita <anki333soul@gmail.com>
2021-12-19 20:16:18 -0700

    added replace image functionality and remove image fuctionality

--- src/app/image-upload/image-upload.component.ts
+++ src/app/image-upload/image-upload.component.ts
@@ -1,25 +1,60 @@
+import { FileHandle } from './../file-handle';
 import { Component, OnInit } from '@angular/core';
-import { FileHandle } from '../file-handle'; 
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-image-upload',
   templateUrl: './image-upload.component.html',
-  styleUrls: ['./image-upload.component.css']
+  styleUrls: ['./image-upload.component.css'],
 })
 export class ImageUploadComponent implements OnInit {
-
   uploadedFiles: FileHandle[] = [];
 
-  constructor() {}  
-  ngOnInit(): void { }  
-  
+  count: number = 0;
+
+  constructor(private sanitizer: DomSanitizer) {}
+  ngOnInit(): void {}
+
   filesDropped(files: FileHandle[]) {
-    if(files.length > 0) {
-      files.forEach(element => {
+    if (files.length > 0) {
+      files.forEach((element) => {
         this.uploadedFiles.push(element);
       });
     }
-      console.log(this.uploadedFiles);
-  }  
+  }
 
+  addAttachment(i: number, files: any) {
+    const file = files[0];
+    const url = this.sanitizer.bypassSecurityTrustUrl(window.URL.createObjectURL(file));
+    this.uploadedFiles[i] = {
+      file,
+      url,
+    };
+  }
+
+  handleClick(i: number) {    
+    const uploadImg = document.getElementById('upload-file');
+    uploadImg.click();
+
+    uploadImg.addEventListener(
+      'change',
+      (e) => {
+        ++this.count;
+        const index = this.count === 2 ? i : -1;
+        if (index > -1) {
+          //@ts-ignore
+          const files = e.target.files;
+          this.addAttachment(i, files);
+          this.count = 0;
+        }
+      },
+      {
+        once: true,
+      }
+    );
+  }
+
+  onDelete(i: number) {
+    this.uploadedFiles.splice(i,1);
+  }
 }
